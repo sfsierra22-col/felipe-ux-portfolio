cab2f890314677af05154cbf22a2d50c0fce9a6f
Sebastian Felipe Sierra Chacon <sfsierra22@gmail.com>
2026-01-29 16:48:36 -0500

    cambio header superior a lateral

--- app/page.tsx
+++ app/page.tsx
@@ -182,7 +182,7 @@ export default function Home() {
       transition
     "
                   >
-                    {role.cta} <span aria-hidden>→</span>
+                    {role.cta} 
                   </Link>
                 </div>
               </div>
